a3773d2287ea5cc6dd80391c98b9fdd34bd6bd3d
bootcamp vinni <alekhyavinni12@gmail.com>
2023-09-25 11:22:34 -0400

    server connection

--- server.js
+++ server.js
@@ -11,5 +11,30 @@ const SequelizeStore=require('connect-session-sequelize')(session.Store)
 const app=express();
 const PORT=process.env.PORT||3001;
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+  
+  app.use(session(sess));
+
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
