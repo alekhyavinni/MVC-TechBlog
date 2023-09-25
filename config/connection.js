318ef39249c8dc485d5a926d43524e7aaf7da4c7
bootcamp vinni <alekhyavinni12@gmail.com>
2023-09-25 18:03:43 -0400

    connection changes

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,9 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-  // Use Heroku Postgres URL
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // For self-signed certificates
-      },
-    },
-  });
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  // Use local MySQL configuration
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -23,7 +13,7 @@ if (process.env.DATABASE_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
+      port: 3306
     }
   );
 }
