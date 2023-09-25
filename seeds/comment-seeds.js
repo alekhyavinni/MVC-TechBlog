3f1d5c508b30d9f14be08be0c24b6f9926feb046
bootcamp vinni <alekhyavinni12@gmail.com>
2023-09-25 13:15:25 -0400

    seeded data

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,20 @@
+const {Comment} = require('../models')
+
+const commentData =[{
+    comment_text:'what is MVC Framework?',
+    user_id:1,
+    post_id:1
+},
+{
+    comment_text: "What are the Cookies?",
+    user_id: 2,
+    post_id: 2
+},
+{
+    comment_text: "What is a template engine?",
+    user_id: 3,
+    post_id: 3
+}
+];
+
+const seedComments=()=>Comment.bulkCreate(commentData)
